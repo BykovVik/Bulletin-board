8096bb03f8435d9a2c991934f9bcaa3c7ad3e824
Vic <piton20091986@gmail.com>
2023-04-21 07:16:03 +0300

    frontend: add header in layout

---
+++ frontend/src/components/header/Header.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import './Header.css'
+import { Button, Form } from "react-bootstrap";
+import logo from './logo.png'
+import {BsFillHouseAddFill, BsFillCarFrontFill, BsTools, BsWrenchAdjustableCircleFill, BsTvFill, BsFillPeopleFill, BsSignpostSplitFill, BsTencentQq} from 'react-icons/bs'
+import {RiTShirtFill} from 'react-icons/ri'
+import {GiCapybara} from "react-icons/gi";
+
+const Header = (props:any) => {
+
+    return (
+        <>
+            <div className="userLine">
+                <div className="logo">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="emptyItem"></div>
+                <div className="authBlock">
+                    <a href="">Sign up/Login</a>
+                </div>
+                <div>
+                    <Button variant="success">Write an ad</Button>
+                </div>
+            </div>
+            <div className="searchLine">
+                <Form>
+                    <Form.Select aria-label="Default select example">
+                        <option>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </Form.Select>
+                    <Form.Select aria-label="Default select example">
+                        <option>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </Form.Select>
+                    <Form.Select aria-label="Default select example">
+                        <option>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </Form.Select>
+                </Form>
+            </div>
+            <div className="category">
+                <button className="categoryItem">
+                    <BsFillHouseAddFill/>
+                    <p>Недвижимость</p>
+                </button>
+                <button className="categoryItem">
+                    <BsFillCarFrontFill/>
+                    <p>Авто</p>
+                </button>
+                <button className="categoryItem">
+                    <BsTools/>
+                    <p>Работа</p>
+                </button>
+                <button className="categoryItem">
+                    <BsWrenchAdjustableCircleFill/>
+                    <p>Услуги</p>
+                </button>
+                <button className="categoryItem">
+                    <RiTShirtFill/>
+                    <p>Одежда</p>
+                </button>
+            </div>
+            <div className="category">
+                <button className="categoryItem">
+                    <BsTvFill/>
+                    <p>Электроника</p>
+                </button>
+                <button className="categoryItem">
+                    <BsFillPeopleFill/>
+                    <p>Отдам даром</p>
+                </button>
+                <button className="categoryItem">
+                    <BsSignpostSplitFill/>
+                    <p>Обменяю</p>
+                </button>
+                <button className="categoryItem">
+                    <BsTencentQq/>
+                    <p>Детское</p>
+                </button>
+                <button className="categoryItem">
+                    <GiCapybara/>
+                    <p>Животные</p>
+                </button>
+            </div>
+        </>
+    )
+}
+
+export default Header
---
+++ frontend/src/components/layout/layout.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
+import Header from '../header/Header'
+
+const layout = (props:any) => {
+
+    return (
+        <Container>
+            <Row>
+                <Col xxl={12} xl={12} lg={12} md={12} sm={12}>
+                    <Header/>
+                </Col>
+            </Row>
+            <Row>
+                <Col xxl={3} xl={3} lg={3} md={3} className='sidebar'>
+                    <h1>Sidebar</h1>
+                </Col>
+                <Col xxl={9} xl={9} lg={9} md={9} className='content'>
+                    {props.children}
+                </Col>
+            </Row>
+            <Row>
+                <Col xxl={12} xl={12} lg={12} md={12} sm={12} className='footer'>
+                    <h1>Footer</h1>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default layout
