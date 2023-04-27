60ea60f6d24da37b29b7a87cfc43fe2402847ce7
Vic <piton20091986@gmail.com>
2023-04-27 08:38:28 +0300

    backend: add mongodb

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,12 @@
 import app from './app';
+import * as dotenv from 'dotenv'
+import dbConnect from './db';
 
-app.listen(3333);
+dotenv.config()
+
+async function main() {
+    await dbConnect();
+}
+  
+main();
+app.listen(process.env.PORT);
