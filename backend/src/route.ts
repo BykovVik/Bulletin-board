84c019e6ddef16c401f68e7d55a8207aeeaed437
Vic <piton20091986@gmail.com>
2023-06-21 08:26:57 +0300

    backend: add login endpoint

--- backend/src/route.ts
+++ backend/src/route.ts
@@ -15,7 +15,6 @@ routes.post('/api/auth/register', async (req, res) => {
     // parse request body
     const {name, last_name, email, password} = req.body
     const birthdate = new Date(req.body.date)
-    console.log(name, last_name, email, password, birthdate)
 
     try {
         // checking that there is no user with such email in the database
@@ -50,6 +49,29 @@ routes.post('/api/auth/register', async (req, res) => {
     } catch (error) {
         console.error(error.message);
         return res.status(500).json({ message: 'Ошибка сервера' });
+    }
+})
+
+routes.post('/api/auth/login', async (req, res) => {
+    const {email, password} = req.body
+
+    try {
+        // checking that there is no user with such email in the database
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            return res.status(400).json({ message: 'Пользователь с таким email не обнаружен' });
+        }
+        // check password from request
+        const hashedPassword = await bcrypt.hash(password, existingUser.password);
+
+        if (existingUser.password === hashedPassword) {
+            return res.status(200).json({ user: existingUser });;
+        } else {
+            return res.status(400).json({ message: 'Пользователь с таким email не обнаружен' });
+        }
+
+    } catch (error:any) {
+        console.error(error.message)
     }
 })
 
